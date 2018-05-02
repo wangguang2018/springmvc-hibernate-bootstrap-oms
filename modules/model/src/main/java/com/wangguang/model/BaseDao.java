65bd83531b9798b9de7795ba88d6d1a3950fb053
wangguang <wangguang@ydd100.cn>
2018-05-02 17:22:16 +0800

    '修改bug'

--- modules/model/src/main/java/com/wangguang/model/BaseDao.java
+++ modules/model/src/main/java/com/wangguang/model/BaseDao.java
@@ -2,6 +2,7 @@ package com.wangguang.model;
 
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.NoRepositoryBean;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.stereotype.Repository;
 
@@ -12,6 +13,7 @@ import java.io.Serializable;
  *
  * @date 2015-05-22
  */
+@NoRepositoryBean
 public interface BaseDao<E, PK extends Serializable> extends CrudRepository<E, PK>, PagingAndSortingRepository<E, PK>, JpaSpecificationExecutor<E> {
 
 
